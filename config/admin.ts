36734fb7fc9c9a902b454f6d8e7f58c375281618
DavidZgz32 <david_zgz_1@hotmail.com>
2025-11-10 08:00:35 +0100

    no message

--- config/admin.ts
+++ config/admin.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_AUTH_SECRET', env('STRAPI_ADMIN_JWT_SECRET', env('ADMIN_JWT_SECRET'))),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
