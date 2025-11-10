74222918d1912e9491eab1788eb87bbd1173965d
DavidZgz32 <david_zgz_1@hotmail.com>
2025-11-10 08:21:40 +0100

    no message

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+export default ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('STRAPI_USERS_PERMISSIONS_JWT_SECRET', env('JWT_SECRET')),
+    },
+  },
+});
